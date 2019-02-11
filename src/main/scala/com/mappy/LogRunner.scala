8bd5040e164c7b99fad0d7de632528fd5e5242df
hero <w@Hero.local>
2019-02-12 00:32:05 +0100

    First version

---
+++ src/main/scala/com/mappy/LogRunner.scala
@@ -0,0 +1,76 @@
+package com.mappy
+
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+
+object LogRunner {
+
+  /** Usage: log [file] */
+  def main(args: Array[String]) {
+    if (args.length < 1) {
+      System.err.println("Usage: Logfile <file>")
+      System.exit(1)
+    }
+
+    val spark = SparkSession
+      .builder
+      .appName("Mappy Log Exercice")
+      .getOrCreate()
+
+
+    val logFile = getClass.getResource(args(0)).getPath
+    val df = spark.read.textFile(logFile).toDF("value")
+    val pattern = "/map/1.0/slab/*/*/*/*/"
+
+    val countDfWithZoom = df.transform(filterWithRegex(col("value"), pattern))
+      .transform(withColumnsFromSplit())
+      .transform(getCountiewModesWithZoom)
+
+
+    countDfWithZoom.show()
+    println(countDfWithZoom.count)
+
+    spark.stop()
+
+  }
+
+
+
+  //Filter unformatted data
+  def filterWithRegex(column: Column, pattern: String)(df:DataFrame): DataFrame = {
+    df.filter(column.rlike(pattern))
+  }
+
+
+  //Split a column value into multiple columns
+  def withColumnsFromSplit()(df: DataFrame): DataFrame = {
+    val headerLength = df.rdd.first.toString.split("/").length
+
+    df.withColumn("value" , split(col("value"), "/"))
+      .select((0 until headerLength)
+        .map(i=>col("value")
+          .getItem(i).as("column_"+ i))
+        :_* )
+  }
+
+
+  //Count distinct viewmodes
+  def getCountiewModes(df: DataFrame) : DataFrame ={
+    df.groupBy("column_4").count()
+  }
+
+
+  //Count distinct viewmodes and collect all the values of columns_6
+  def getCountiewModesWithZoom(df: DataFrame) : DataFrame ={
+    df.withColumnRenamed("column_4","ViewMode")
+      .groupBy("ViewMode")
+      .agg(count("ViewMode").as("CountViewModes"),concat_ws(",",
+        collect_set("column_6")).as("Zoom"))
+  }
+
+
+
+
+
+
+}
---
+++ src/test/scala/LogTest.scala
@@ -0,0 +1,54 @@
+package com.mappy
+import com.mappy.LogRunner._
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.functions._
+import org.scalatest.{FunSuite, SequentialNestedSuiteExecution}
+
+
+class LogTest extends FunSuite with SequentialNestedSuiteExecution with SparkSessionTestWrapper  {
+
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
+
+
+  val logFile = getClass.getResource("/tornik-map-20171006.10000.tsv").getPath
+
+  val df = spark.read.textFile(logFile).toDF("value")
+  val pattern = "/map/1.0/slab/*/*/*/*/"
+
+  test("Test 0: Read Raw File ") {
+    df.show
+    println(df.count)
+
+  }
+
+  test("Test 1 : Filtred DF") {
+    val filtredDf= df.transform(filterWithRegex(col("value"),pattern))
+    filtredDf.show()
+    println(filtredDf.count)
+  }
+
+  test("Test 2: Split DF into multiple Columns") {
+    val splittedDf =  df.transform(filterWithRegex(col("value"),pattern))
+      .transform(withColumnsFromSplit())
+    splittedDf.show()
+    println(splittedDf.count)
+  }
+
+  test("Test 3: Mode View with count") {
+    val countDf= df.transform(filterWithRegex(col("value"),pattern))
+      .transform(withColumnsFromSplit())
+      .transform(getCountiewModes)
+    countDf.show()
+    println(countDf.count)
+  }
+
+  test("Test 4: Mode View with count and Zoom values") {
+    val countDfWithZoom = df.transform(filterWithRegex(col("value"),pattern))
+      .transform(withColumnsFromSplit())
+      .transform(getCountiewModesWithZoom)
+    countDfWithZoom.show()
+    println(countDfWithZoom.count)
+  }
+
+}
